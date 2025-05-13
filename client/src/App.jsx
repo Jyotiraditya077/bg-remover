6c512619097e648b6a425a7baf725bf270a3f7b6
jyotiraditya swain <jyotiradityaswain123@gmail.com>
2025-05-14 02:52:58 +0530

    updated ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Result from './pages/Result';
@@ -11,18 +11,18 @@ import Verify from './pages/Verify';
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-slate-50'>
-      <ToastContainer position='bottom-right' />
+    <div className="min-h-screen bg-customColor"> {/* Applying custom color */}
+      <ToastContainer position="bottom-right" />
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/result' element={<Result />} />
-        <Route path='/buy' element={<BuyCredit />} />
-        <Route path='/verify' element={<Verify />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+        <Route path="/verify" element={<Verify />} />
       </Routes>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -2,17 +2,18 @@ import logo from './logo.svg'
 import logo_icon from './logo_icon.svg'
 import arrow_icon from './arrow_icon.svg'
 import header_img from './header_img.png'
+import header_img2 from './header_img2.png'
 import remove_bg_icon from './remove_bg_icon.svg'
 import upload_btn_icon from './upload_btn_icon.svg'
 import upload_icon from './upload_icon.svg'
 import download_icon from './download_icon.svg'
 import image_w_bg from './image_w_bg.png'
 import image_wo_bg from './image_wo_bg.png'
-import facebook_icon from './facebook_icon.svg'
-import google_plus_icon from './google_plus_icon.svg'
+import github_icon from './github_icon.svg'
+import contact_icon from './contact_icon.svg'
 import twitter_icon from './twitter_icon.svg'
 import profile_img_1 from './profile_img_1.png'
-import profile_img_2 from './profile_img_2.png'
+import profile_img_2 from './profile_img_2.jpg'
 import credit_icon from './credit_icon.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
@@ -22,13 +23,14 @@ export const assets = {
     logo_icon,
     arrow_icon,
     header_img,
+    header_img2,
     remove_bg_icon,
     upload_icon,
     download_icon,
     image_w_bg,
     image_wo_bg,
-    facebook_icon,
-    google_plus_icon,
+    github_icon,
+    contact_icon,
     twitter_icon,
     upload_btn_icon,
     credit_icon,
